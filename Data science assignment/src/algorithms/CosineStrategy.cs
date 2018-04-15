979199fab789d80dbcf8f97aa085e8259ebdee98
KajdeMunter <kajdemunter@hotmail.com>
2018-04-15 14:27:14 +0200

    Consine strategy

--- Data science assignment/src/algorithms/CosineStrategy.cs
+++ Data science assignment/src/algorithms/CosineStrategy.cs
@@ -10,7 +10,22 @@ namespace Data_science_assignment.src.algorithms
     {
         public Double Execute(UserPreferences rating1, UserPreferences rating2)
         {
-            throw new NotImplementedException();
+            float sumOfXmultipliedByY = 0F;
+            float sumOfSquaredX = 0F;
+            float sumOfSquaredY = 0F;
+
+            foreach (KeyValuePair<int, float> kvprating1 in rating1.ratings)
+            {
+                foreach (KeyValuePair<int, float> kvprating2 in rating2.ratings)
+                {
+                    sumOfXmultipliedByY += kvprating1.Value * kvprating2.Value;
+                    sumOfSquaredX += kvprating1.Value * kvprating1.Value;
+                    sumOfSquaredY += kvprating2.Value * kvprating2.Value;
+
+                }
+            }
+
+            return sumOfXmultipliedByY / (Math.Sqrt(sumOfSquaredX) * Math.Sqrt(sumOfSquaredY));
         }
     }
 }
