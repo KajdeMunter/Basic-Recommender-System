8a816a80966f1fa2a36cd9597087a8f720918828
KajdeMunter <kajdemunter@hotmail.com>
2019-04-14 15:45:31 +0200

    Fixed neighbour rating prediction not resetting numerator and denumerator

--- Data science assignment/src/algorithms/PredictRating.cs
+++ Data science assignment/src/algorithms/PredictRating.cs
@@ -22,40 +22,38 @@ namespace Data_science_assignment.src.algorithms
         /// <param name="targetUser"></param>
         public void Calculate(List<Tuple<double, UserPreference>> neighbours, UserPreference targetUser)
         {
-            double numerator = 0.0;
-            double denominator = 0.0;
 
-            List<UserPreference> neighbourPreferences = new List<UserPreference>();
 
+            List<int> productsToRate = new List<int>();
+
+            // Add products that neighbours have rated
             foreach (Tuple<double, UserPreference> tuple in neighbours)
             {
-                neighbourPreferences.Add(tuple.Item2);
+                foreach (int pid in tuple.Item2.ratings.Keys)
+                {
+                    if (!productsToRate.Contains(pid)) productsToRate.Add(pid);
+                }
             }
 
-            // Get the products all neighbours have rated
-            HashSet<int> targetHashSet = new HashSet<int>(_data.loader.getRatingsWithoutZero(neighbourPreferences.First()).Keys);
-
-            foreach (Tuple<double, UserPreference> neighbourPreference in neighbours.Skip(1))
-            {
-                targetHashSet.IntersectWith(_data.loader.getRatingsWithoutZero(neighbourPreference.Item2).Keys);
-            }
-
-            List<int> productsInCommon = targetHashSet.ToList();
-            
-            
             // Remove the products that the user has already rated so we have a list of products that we can predict
             foreach (int pid in _data.loader.getRatingsWithoutZero(targetUser).Keys)
             {
-                productsInCommon.Remove(pid);
+                productsToRate.Remove(pid);
             }
 
             // Predict rating
-            foreach (int pid in productsInCommon)
+            foreach (int pid in productsToRate)
             {
+                double numerator = 0.0;
+                double denominator = 0.0;
+
                 foreach (Tuple<double, UserPreference> kvp in neighbours)
                 {
-                    numerator += (kvp.Item1 * kvp.Item2.ratings[pid]);
-                    denominator += kvp.Item1;
+                    if (_data.loader.getRatingsWithoutZero(kvp.Item2).ContainsKey(pid))
+                    {
+                        numerator += kvp.Item1 * _data.loader.getRatingsWithoutZero(kvp.Item2)[pid];
+                        denominator += kvp.Item1;
+                    }
                 }
 
                 Console.WriteLine($"Predicted rating for {pid} is {numerator / denominator}");
